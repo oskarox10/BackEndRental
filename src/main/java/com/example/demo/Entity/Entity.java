bfb65e9644912bc8e2b4f2d217b9348507754b1e
Oskar <oskarox10@gmail.com>
2024-04-24 13:33:27 +0200

    dodanie klasy entity i polaczenia z DB

--- src/main/java/com/example/demo/Entity/Entity.java
+++ src/main/java/com/example/demo/Entity/Entity.java
@@ -1,4 +1,18 @@
 package com.example.demo.Entity;
 
+
 public class Entity {
+
+    private int id;
+
+    private String brand;
+    private String model;
+    private int manufacturingYear;
+
+    private String carStatus;
+
+
+
+
+
 }
