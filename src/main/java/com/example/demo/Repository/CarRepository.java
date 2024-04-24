eeb5a34886efa474d5c581f8f87dae66f4495343
Oskar <oskarox10@gmail.com>
2024-04-24 13:39:14 +0200

    dodanie JPA repository, aby mozna bylo uzywac CRUDowych metod

---
+++ src/main/java/com/example/demo/Repository/CarRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.CarEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<CarEntity, Integer> {
+}
