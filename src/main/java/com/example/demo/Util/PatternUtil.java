637237ccc2de7ec9e72cbc8254b26be6e10762f7
Oskar <oskarox10@gmail.com>
2024-04-25 17:49:12 +0200

    Dodanie metoday ktora generuje UUID dla samochodów.

---
+++ src/main/java/com/example/demo/Util/PatternUtil.java
@@ -0,0 +1,21 @@
+package com.example.demo.Util;
+
+import java.util.UUID;
+
+public class PatternUtil {
+
+    public static final String CAR_ID_PREFIX = "CAR-";
+    public static final String UUID_REGEX = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
+    public static final String CAR_ID_REGEX = CAR_ID_PREFIX + UUID_REGEX;
+
+
+    public static String generateId()
+    {
+        String id = CAR_ID_PREFIX + UUID.randomUUID().toString();
+        if (!id.matches(UUID_REGEX)) {
+            throw new IllegalArgumentException("ID is wrong");
+        }
+        return id;
+    }
+
+}
